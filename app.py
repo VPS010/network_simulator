733198f881fe5a8257164a7dfda761a8a964268e
Vinay Prakash Senapati <vinayprakashsenapati01@gmail.com>
2024-04-05 17:01:58 +0530

    V2 all topology,plot n msg passing  added

--- app.py
+++ app.py
@@ -1,88 +1,197 @@
-import io
-from flask import Flask, render_template, request, send_file
-from network_simulation import *
+from flask import Flask, render_template, request, redirect, url_for
 import matplotlib.pyplot as plt
 import networkx as nx
+import os
 
 app = Flask(__name__)
 
-# Function to generate and save the network diagram
-def generate_network_diagram(devices, hub, topology_name):
-    G = nx.Graph()
+class Device:
+    def __init__(self, device_id):
+        self.device_id = device_id
+        self.connected_devices = []
+    
+    def connect(self, other_device):
+        if other_device not in self.connected_devices:
+            self.connected_devices.append(other_device)
+            other_device.connected_devices.append(self)
+            print(f"Devices {self.device_id} and {other_device.device_id} connected.")
+    
+    def send_data(self, data):
+        print(f"Device {self.device_id} sending data: {data}")
+        for device in self.connected_devices:
+            device.receive_data(data)
+    
+    def receive_data(self, data):
+        print(f"Device {self.device_id} received data: {data}")
 
-    # Add devices to the graph
-    for device in devices:
-        G.add_node(device.name)
 
-    # Add connections between devices and hub
-    for device in devices:
-        G.add_edge(hub.name, device.name)
+class Hub(Device):
+    def __init__(self, hub_id):
+        super().__init__(hub_id)
+    
+    def connect_to_hub(self, device):
+        self.connect(device)
+    
+    def broadcast(self, data):
+        print(f"Hub {self.device_id} broadcasting data: {data}")
+        for device in self.connected_devices:
+            device.receive_data(data)
+    
+    def receive_data(self, data):
+        print(f"Hub {self.device_id} received data: {data}")
+        self.broadcast(data)
 
-    # Generate the network diagram
-    pos = nx.spring_layout(G)
-    nx.draw(G, pos, with_labels=True, node_size=700, node_color='lightblue', font_size=10, font_weight='bold')
-    plt.title(f"Network Topology: {topology_name}")
-    plt.tight_layout()
 
-    # Save the diagram to a BytesIO object
-    img_bytes = io.BytesIO()
-    plt.savefig(img_bytes, format='png')
-    img_bytes.seek(0)
-    plt.close()
+class Switch(Device):  # Switch is also a Device
+    def __init__(self, switch_id):
+        super().__init__(switch_id)
+    
+    def connect_to_switch(self, device):
+        self.connect(device)
 
-    return img_bytes
+    def send_data(self, data, receiver_id):
+        for device in self.connected_devices:
+            if device.device_id == receiver_id:
+                device.receive_data(data)
+                print(f"Switch {self.device_id} forwarded data to {device.device_id}")
+                return
+        print(f"Receiver device {receiver_id} not connected to switch {self.device_id}")
 
-# Function to create a star topology with one hub at the center
-def create_star_topology(devices, hub_name):
-    hub = Hub(hub_name)
-    for device in devices:
-        hub.connect_device(device)
-    return hub
 
-# Function to create a point-to-point topology
-def create_point_to_point_topology(devices):
-    # Connect devices in a line (point-to-point)
-    for i in range(len(devices) - 1):
-        devices[i].connect_to(devices[i+1])
+class Topology:
+    def __init__(self):
+        self.devices = []
+        self.graph = nx.Graph()
+    
+    def add_device(self, device):
+        self.devices.append(device)
+        self.graph.add_node(device.device_id)
+    
+    def create_connection(self, device1, device2):
+        device1.connect(device2)
+        self.graph.add_edge(device1.device_id, device2.device_id)
+    
+    def create_star_topology(self, devices, hub):
+        self.add_device(hub)
+        for device in devices:
+            self.add_device(device)
+            self.create_connection(device, hub)
+    
+    def create_bus_topology(self, devices, switches):
+        self.add_device(switches[0])  # Connect the first switch to the hub
+        self.create_connection(switches[0], devices[0])
+        self.add_device(devices[0])
+        for i in range(1, len(switches)):
+            self.add_device(switches[i])
+            self.create_connection(switches[i], switches[i-1])  # Connect switches in a line
+            self.create_connection(switches[i], devices[i])
+            self.add_device(devices[i])
+    
+    def create_ring_topology(self, devices, switches):
+        for device, switch in zip(devices, switches):
+            self.add_device(switch)
+            self.add_device(device)
+            self.create_connection(device, switch)
+        for i in range(len(switches)):
+            self.create_connection(switches[i], switches[(i + 1) % len(switches)])  # Circular connection
+    
+    def create_mesh_topology(self, devices, switches):
+        for device, switch in zip(devices, switches):
+            self.add_device(switch)
+            self.add_device(device)
+            self.create_connection(device, switch)
+            for other_switch in switches:
+                if other_switch != switch:
+                    self.create_connection(switch, other_switch)
+    
+    def plot_topology(self):
+        pos = nx.spring_layout(self.graph)  # Layout for graph visualization
+        nx.draw(self.graph, pos, with_labels=True, node_size=800, node_color='skyblue', font_size=10, font_weight='bold')
+        plt.title('Network Topology')
+        plt.savefig('static/topology.png')  # Save the plot image
+        plt.close()
 
+
+class Simulation:
+    def __init__(self):
+        self.topology = Topology()
+    
+    def create_network(self, num_devices, topology_type):
+        if topology_type.lower() == 'star':
+            devices = [Device(f"Device{i+1}") for i in range(num_devices)]
+            hub = Hub("Hub1")
+            self.topology.create_star_topology(devices, hub)
+        elif topology_type.lower() == 'bus':
+            num_switches = num_devices
+            switches = [Switch(f"Switch{i+1}") for i in range(num_switches)]
+            devices = [Device(f"Device{i+1}") for i in range(num_devices)]
+            self.topology.create_bus_topology(devices, switches)
+        elif topology_type.lower() == 'ring':
+            num_switches = num_devices
+            switches = [Switch(f"Switch{i+1}") for i in range(num_switches)]
+            devices = [Device(f"Device{i+1}") for i in range(num_devices)]
+            self.topology.create_ring_topology(devices, switches)
+        elif topology_type.lower() == 'mesh':
+            num_switches = num_devices
+            switches = [Switch(f"Switch{i+1}") for i in range(num_switches)]
+            devices = [Device(f"Device{i+1}") for i in range(num_devices)]
+            self.topology.create_mesh_topology(devices, switches)
+        else:
+            print("Invalid topology type.")
+    
+    def check_message_path(self, sender_id, receiver_id):
+        if sender_id not in self.topology.graph.nodes or receiver_id not in self.topology.graph.nodes:
+            print("Invalid sender or receiver device ID.")
+            return False
+        
+        try:
+            path = nx.shortest_path(self.topology.graph, source=sender_id, target=receiver_id)
+            print("Path found:", " -> ".join(path))
+            return path
+        except nx.NetworkXNoPath:
+            print("No path found between the sender and receiver.")
+            return False
+    
+    def send_message(self, path, message, receiver_id):
+        for i in range(len(path)-1):
+            sender = next(device for device in self.topology.devices if device.device_id == path[i])
+            receiver = next(device for device in self.topology.devices if device.device_id == path[i+1])
+            if isinstance(sender, Switch):
+                sender.send_data(message, receiver_id)
+            else:
+                sender.send_data(message)
+            print(f"Message sent from {sender.device_id} to {receiver.device_id}")
+    
+    def run_simulation(self, num_devices, topology_type, sender_id, receiver_id, message):
+        self.create_network(num_devices, topology_type)
+        print("Devices in the network:", [device.device_id for device in self.topology.devices])
+        
+        path = self.check_message_path(sender_id, receiver_id)
+        if path:
+            self.topology.plot_topology()  # Plot the network graph
+            self.send_message(path, message, receiver_id)
+            return True, path
+        else:
+            return False, None
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         num_devices = int(request.form['num_devices'])
-        devices = []
-        for i in range(num_devices):
-            name = request.form[f'name{i+1}']
-            device_type = request.form[f'type{i+1}']
-            if device_type == 'EndDevice':
-                devices.append(EndDevice(name))
-            elif device_type == 'Hub':
-                devices.append(Hub(name))
+        topology_type = request.form['topology_type']
+        sender_id = request.form['sender_id']
+        receiver_id = request.form['receiver_id']
+        message = request.form['message']
+        
+        simulation = Simulation()
+        success, path = simulation.run_simulation(num_devices, topology_type, sender_id, receiver_id, message)
+        if success:
+            return render_template('index.html', plot_available=True, path=path, message=message)
+        else:
+            return render_template('index.html', plot_available=False, error_message="No path found between the sender and receiver.")
+    return render_template('index.html', plot_available=False)
 
-        topology_choice = int(request.form['topology_choice'])
-        topology_name = request.form['topology_name']
-        hub = None  # Initialize hub variable
 
-        if topology_choice == 1:
-            hub = create_star_topology(devices, topology_name)
-        elif topology_choice == 2:
-            create_point_to_point_topology(devices)
-            hub = devices[0]  # Set the hub to the first device in point-to-point topology
-        elif topology_choice == 3:
-            hub = create_bus_topology(devices)
-        elif topology_choice == 4:
-            create_ring_topology(devices)
-        elif topology_choice == 5:
-            create_mesh_topology(devices)
-
-        if hub:
-            # Generate and save the network diagram
-            diagram_bytes = generate_network_diagram(devices, hub, hub.name)
-            # Send the diagram file as a response
-            return send_file(diagram_bytes, mimetype='image/png')
-
-    return render_template('index.html')
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
